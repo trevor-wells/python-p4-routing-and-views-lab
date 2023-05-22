abc4df8a66318b738e17da838a19259a2fbeab2f
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-22 14:42:59 -0600

    final commit

--- server/app.py
+++ server/app.py
@@ -24,13 +24,15 @@ def count(my_number):
 @app.route('/math/<int:num1>/<operation>/<int:num2>')
 def math(num1, operation, num2):
     if operation == '+':
-        return num1 + num2
+        return str(num1 + num2)
     elif operation == '-':
-        return num1 - num2
+        return str(num1 - num2)
     elif operation == '*':
-        return num1 * num2
+        return str(num1 * num2)
     elif operation == 'div':
-        return num1 / num2
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
